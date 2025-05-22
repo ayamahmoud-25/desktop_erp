1cff16c6f9b284afbec5ed3979d2572fd32647a2
ayamahmoud-25 <ayamahmoudmsd005@gmail.com>
2025-05-22 14:11:49 +0300

    fix import error related to floder learn

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:desktop_erp_4s/ui/stockTransaction/showTransaction/show_transact
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'learn/futuerbuilderproviderlogin/new_login_page.dart';
 
 void main() {
   runApp(
